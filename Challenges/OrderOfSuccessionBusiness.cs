f4bdb450909945ef7b2a620cc05581619624cab0
CHOLLET-EXT Alexis <alexis.chollet-ext@fnacdarty.com>
2018-07-26 09:29:27 +0200

    Initial commit

---
+++ Challenges/OrderOfSuccessionBusiness.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Challenges
+{
+    #region classes definition
+
+    public class FamilyTreeNode
+    {
+        public Person Person { get; set; }
+        public List<Person> Children { get; set; }
+        public Person Parent { get; set; }
+
+        public FamilyTreeNode(Person person)
+        {
+            Person = person;
+            Children = new List<Person>();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+        
+    }
+
+    #endregion
+
+    #region business logic
+    public class OrderOfSucessionBusiness
+    {
+
+    }
+    #endregion
+}
---
+++ ChallengesTest/OrderOfSuccessionBusinessTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChallengesTest
+{
+    [TestClass]
+    public class OrderOfSuccessionBusinessTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
