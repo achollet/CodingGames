3fc6b7a717be965b12216de78874a40a36847c59
A. Chollet <alpchollet+github@gmail.com>
2018-07-27 08:05:30 +0200

    Putting in place the business part

--- Challenges/OrderOfSuccessionBusiness.cs
+++ Challenges/OrderOfSuccessionBusiness.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Challenges
@@ -8,21 +9,24 @@ namespace Challenges
     public class FamilyTreeNode
     {
         public Person Person { get; set; }
-        public List<Person> Children { get; set; }
+        public List<FamilyTreeNode> Children { get; set; }
         public Person Parent { get; set; }
 
         public FamilyTreeNode(Person person)
         {
             Person = person;
-            Children = new List<Person>();
+            Children = new List<FamilyTreeNode>();
         }
     }
 
     public class Person
     {
         public string Name { get; set; }
-        public int YearOfBirth { get; set; }
-        
+        public string ParentName { get; set; }
+        // public int YearOfBirth { get; set; }
+        // public string YearOfDeath { get; set; }
+        // public string Religion {get ; set ;}
+        // public string Gender { get; set; }
     }
 
     #endregion
@@ -30,7 +34,35 @@ namespace Challenges
     #region business logic
     public class OrderOfSucessionBusiness
     {
+        public FamilyTreeNode CreateFamilyTreeFromListOfFamilyMembers(List<Person> familyMembers)
+        {
+            var headOfFamilyTree = familyMembers.First(m => (String.IsNullOrEmpty(m.ParentName) || String.IsNullOrWhiteSpace(m.ParentName)));
 
+            var headOfFamilyTreeNode = new FamilyTreeNode(new Person());
+
+            if (headOfFamilyTree != null)
+            {
+                headOfFamilyTreeNode.Person.Name = headOfFamilyTree.Name;
+            }
+
+            PopulateChildrenOfNode(headOfFamilyTree, familyMembers);
+
+
+            return headOfFamilyTreeNode
+        }
+
+        private void PopulateChildrenOfNode(Person familyTreeNode, IEnumerable<Person> familyMembers)
+        {
+            var children = familyMembers.Where(m => m.ParentName == familyTreeNode.Name);
+
+            if(children.Any())
+            {
+                foreach(var child in children)
+                {
+                    var childNode = new FamilyTreeNode(new Person)
+                }
+            }
+        }
     }
     #endregion
 }
--- ChallengesTest/OrderOfSuccessionBusinessTest.cs
+++ ChallengesTest/OrderOfSuccessionBusinessTest.cs
@@ -1,13 +1,36 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Challenges;
 
 namespace ChallengesTest
 {
     [TestClass]
     public class OrderOfSuccessionBusinessTest
     {
+        private List<Person> _familyMembers;
+        private OrderOfSucessionBusiness _orderOfSuccessionBusiness;
+
+        [[TestInitialize]
+        public void InitializeOrderOfSuccessionBusinessTest()
+        {
+            _orderOfSuccessionBusiness = new OrderOfSucessionBusiness();
+
+            _familyMembers = new List<Person>
+            {
+                new Person{Name = "Elizabeth", ParentName = string.Empty},
+                new Person{Name = "Charles", ParentName = "Elizabeth"},
+                new Person{Name = "William", ParentName = "Chales"}
+            };
+        }]
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void CreateFamilyTreeFromListOfFamilyMembers_Test_ReturnOk()
         {
         }
     }
 }
+
+// Elizabeth - 1926 - Anglican F
+// Charles Elizabeth 1948 - Anglican M
+// William Charles 1982 - Anglican M
