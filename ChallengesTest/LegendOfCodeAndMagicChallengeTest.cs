103b9e7157f9cfa0abbb1963a249461dd03ab06b
CHOLLET-EXT Alexis <alexis.chollet-ext@fnacdarty.com>
2018-07-31 17:03:04 +0200

    Add Method PickCardFromDeck

--- ChallengesTest/LegendOfCodeAndMagicChallengeTest.cs
+++ ChallengesTest/LegendOfCodeAndMagicChallengeTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Challenges.LegendOfCodeAndMagic;
+using System.Collections.Generic;
 
 namespace ChallengesTest
 {
@@ -7,17 +8,27 @@ namespace ChallengesTest
     public class LegendOfCodeAndMagicChallengeTest
     {
         private LegendOfCodeAndMagicChallenge _legendOfCodeAndMagicChallenge;
+        private IEnumerable<Card> _boardDeck;
 
         [TestInitialize]
         public void Init()
         {
             _legendOfCodeAndMagicChallenge = new LegendOfCodeAndMagicChallenge();
+            _boardDeck = new List<Card>
+            {
+                new Card{ CardNumber = 0, AttackPoints = 2},
+                new Card{ CardNumber = 1, AttackPoints = 5},
+                new Card{ CardNumber = 2, AttackPoints = 9},
+            };
         }
 
         [TestMethod]
-        public void Test1()
+        public void PickCardFromBoardDeck()
         {
-            Assert.IsTrue(true);
+            var result = _legendOfCodeAndMagicChallenge.PickCardFromBoardDeck(_boardDeck);
+            var expected = "PICK 2";
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
